50d6343a1e95ac572b526b2788fb8695679e6f91
hoess345 <s2210307068@fhooe.at>
2024-06-20 14:59:55 +0200

    commit changes from exercise3

--- src/main/java/spw4/connectfour/ConnectFourImpl.java
+++ src/main/java/spw4/connectfour/ConnectFourImpl.java
@@ -1,25 +1,126 @@
 package spw4.connectfour;
 
 public class ConnectFourImpl implements ConnectFour {
+    private Player playerOnTurn;
+    private final Character[][] board = new Character[6][7];
+
+
     public ConnectFourImpl(Player playerOnTurn) {
+        this.playerOnTurn = playerOnTurn;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) board[i][j] = '.';
+        }
     }
 
     public Player getPlayerAt(int row, int col) {
-        return Player.none;
+        if (board[row][col] == 'R') return Player.red;
+        else if (board[row][col] == 'Y') return Player.yellow;
+        else return Player.none;
     }
+
     public Player getPlayerOnTurn() {
-        return Player.yellow;
+        return playerOnTurn;
     }
+
     public boolean isGameOver() {
+        boolean gameboardisfull = true;
+
+        for (int i = 0; i < 6; i++) {
+            if (!gameboardisfull) break;
+            for (int j = 0; j < 7; j++) {
+
+                if (board[i][j] == '.') {
+                    gameboardisfull = false;
+                    break;
+                }
+            }
+        }
+        if (gameboardisfull) return true;
+        else return checkforwinner();
+    }
+
+    private boolean checkforwinner() {
+        //check if in board are 4 Connected discs
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (board[i][j] != '.') {
+                    //check vertical
+                    if (i + 3 < 6) {
+                        if (board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j] && board[i][j] == board[i + 3][j]) {
+                            return true;
+                        }
+                    }
+                    //check horizontal
+                    if (j + 3 < 7) {
+                        if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] && board[i][j] == board[i][j + 3]) {
+                            return true;
+                        }
+                    }
+                    //check diagonal
+                    if (i - 3 >= 0 && j + 3 < 7) {
+                        if (board[i][j] == board[i - 1][j + 1] && board[i][j] == board[i - 2][j + 2] && board[i][j] == board[i - 3][j + 3]) {
+                            return true;
+                        }
+                    }
+                    //check other diagonal
+                    if (i - 3 >= 0 && j - 3 >= 0) {
+                        if (board[i][j] == board[i - 1][j - 1] && board[i][j] == board[i - 2][j - 2] && board[i][j] == board[i - 3][j - 3]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
+
     public Player getWinner() {
-        return Player.none;
+        if (!isGameOver()) return Player.none;
+        else if (!checkforwinner()) return Player.none;
+        return playerOnTurn == Player.yellow ? Player.red : Player.yellow;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        output.append("\nPlayer:").append(playerOnTurn.toString()).append("\n");
+        for (int i = 0; i < 6; i++) {
+            output.append("|");
+            for (int j = 0; j < 7; j++) {
+                output.append(" ");
+                output.append(board[i][j]);
+                output.append(" ");
+            }
+            output.append("|\n");
+        }
+        return output.toString();
     }
 
     public void reset(Player playerOnTurn) {
+        this.playerOnTurn = playerOnTurn;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                board[i][j] = '.';
+            }
+        }
     }
 
     public void drop(int col) {
+        if (col < 0 || col > 6) throw new IllegalArgumentException("Column must be between 0 and 6");
+        if (isGameOver()) throw new IllegalStateException("Game is over");
+        else {
+            for (int i = 5; i >= 0; i--) {
+                if (board[i][col] == '.') {
+                    if (playerOnTurn == Player.red) {
+                        board[i][col] = 'R';
+                        playerOnTurn = Player.yellow;
+                    } else {
+                        board[i][col] = 'Y';
+                        playerOnTurn = Player.red;
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
--- src/test/java/spw4/connectfour/ConnectFourImplTests.java
+++ src/test/java/spw4/connectfour/ConnectFourImplTests.java
@@ -2,16 +2,179 @@ package spw4.connectfour;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.*;
-import org.junit.jupiter.params.*;
-import org.junit.jupiter.params.provider.*;
-import org.mockito.*;
 import org.mockito.junit.jupiter.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 
 @ExtendWith(MockitoExtension.class)
 class ConnectFourImplTests {
+    ConnectFourImpl game;
 
+    @BeforeEach
+    public void initialize() {
+        game = new ConnectFourImpl(Player.yellow);
+    }
+
+    @Test
+    public void InstanceOfConnectFourIsNotNull() {
+        assertNotNull(game);
+    }
+
+    @Test
+    public void StartAGameAndLookForPlayer() {
+        assertEquals(Player.yellow, game.getPlayerOnTurn());
+    }
+
+    @Test
+    public void getPlayerAtEqualsNone() {
+        assertEquals(Player.none, game.getPlayerAt(0, 0));
+    }
+
+    @Test
+    public void StartGameDropDiscAndLook() {
+        game.drop(0);
+        assertEquals(Player.yellow, game.getPlayerAt(5, 0));
+    }
+
+    @Test
+    public void LookForOutputAfterDrop() {
+        assertEquals("\nPlayer:" + game.getPlayerOnTurn().toString() + "\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n", game.toString());
+        game.drop(0);
+        assertEquals("\nPlayer:" + game.getPlayerOnTurn().toString() + "\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| .  .  .  .  .  .  . |\n" +
+                "| Y  .  .  .  .  .  . |\n", game.toString());
+    }
+
+    @Test
+    public void DropTwoDiscsInSameColumn() {
+        game.drop(0);
+        game.drop(0);
+        assertEquals(Player.yellow, game.getPlayerAt(5, 0));
+        assertEquals(Player.red, game.getPlayerAt(4, 0));
+    }
+
+    @Test
+    public void getPlayerOnSecondTurn() {
+        game.drop(0);
+        assertEquals(Player.red, game.getPlayerOnTurn());
+    }
+
+    @Test
+    public void ResetGame() {
+        game.drop(0);
+        game.drop(0);
+        game.reset(Player.yellow);
+        assertEquals(Player.yellow, game.getPlayerOnTurn());
+        assertEquals(Player.none, game.getPlayerAt(5, 0));
+    }
+
+    @Test
+    public void DropTillGameOver() {
+        game.drop(0);
+        game.drop(1);
+        game.drop(0);
+        game.drop(1);
+        game.drop(0);
+        game.drop(1);
+        game.drop(0);
+        assertTrue(game.isGameOver());
+    }
+
+    @Test
+    public void DropDiscAfterGameOverException() {
+        game.drop(0);
+        game.drop(0);
+        game.drop(1);
+        game.drop(1);
+        game.drop(2);
+        game.drop(2);
+        game.drop(3);
+        assertThrows(IllegalStateException.class, () -> game.drop(3));
+    }
+
+    @Test
+    public void CheckForWinnerRightDirection() {
+        game.drop(0);
+        game.drop(0);
+        game.drop(1);
+        game.drop(1);
+        game.drop(2);
+        game.drop(2);
+        game.drop(3);
+        assertEquals(Player.yellow, game.getWinner());
+    }
+
+    @Test
+    public void CheckForWinnerRightUpperDirection() {
+        game.drop(0);
+        game.drop(1);
+        game.drop(1);
+        game.drop(2);
+        game.drop(2);
+        game.drop(3);
+        game.drop(2);
+        game.drop(3);
+        game.drop(3);
+        game.drop(1);
+        game.drop(3);
+        System.out.println(game.toString());
+        assertEquals(Player.yellow, game.getWinner());
+    }
+
+    @Test
+    public void CheckForWinnerLeftUpperDirection() {
+        game.drop(6);
+        game.drop(5);
+        game.drop(5);
+        game.drop(4);
+        game.drop(4);
+        game.drop(3);
+        game.drop(4);
+        game.drop(3);
+        game.drop(3);
+        game.drop(4);
+        game.drop(3);
+        assertEquals(Player.yellow, game.getWinner());
+    }
+
+    @Test
+    public void CheckForWinnerWhileGameIsOngoing() {
+        game.drop(0);
+        game.drop(1);
+        game.drop(1);
+        game.drop(2);
+        assertEquals(Player.none, game.getWinner());
+    }
+
+    @Test
+    public void WrongColumnDrop() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> game.drop(-1)),
+                () -> assertThrows(IllegalArgumentException.class, () -> game.drop(7))
+        );
+    }
+
+    @Test
+    public void CheckIfGameIsOverWhenBoardIsFull() {
+        int[] moves = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 4, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6};
+
+        for (int move : moves) {
+            game.drop(move);
+            System.out.println(game.toString());
+        }
+        assertAll(
+                () -> assertEquals(Player.none, game.getWinner()),
+                () -> assertTrue(game.isGameOver())
+        );
+    }
 }
